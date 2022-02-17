57f5ab0bbd9248ff0d2c1bf27f9ff20cf1df92bd
ZuandaBaransyahPutra <zuandabaransyahputra@gmail.com>
2022-02-17 17:32:59 +0700

    Gpt3_websites

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Footer, Blog, Header, Possibility, WhatGPT3, Features } from "./containers"
+import { Brand, Cta, Navbar } from "./components"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className='App'>
+            <div className='gradient__bg'>
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <Cta />
+            <Blog />
+            <Footer />
+        </div>
+    )
 }
-
-export default App;
